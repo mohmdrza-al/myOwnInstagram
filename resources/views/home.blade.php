e00a8e7a5358251d1b47d5c5d4b3d74e736002fc
mohmdrza <m.alagheband@gmail.com>
2022-08-18 11:37:04 +0430

    UI has been edited.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="container">
+        <div class="row">
+            <div class="col p-4">
+                <img src="https://dkstatics-public.digikala.com/digikala-brands/af664f0bcf9bc412b741c618e6e52fc2d090198f_1619269008.png?x-oss-process=image/resize,m_lfit,h_160,w_160/quality,q_80" class="rounded-circle" alt="">
+            </div>
+            <div class="col-9 p-5">
+                <div><h1>mohmdrza_al</h1></div>
+                <div class="d-flex">
+                    <div><strong>153</strong> post</div>
+                    <div class="px-5"><strong>153</strong> followers</div>
+                    <div><strong>153</strong> following</div>
                 </div>
+                <div class="pt-4"><strong>mohmdrza_al</strong></div>
+                <div>mohmdrza_al bio</div>
+                <div><a href="#">mohmdrza_al_link</a></div>
             </div>
         </div>
+        <div class="row pt-4">
+            <div class="col-4"><img src="https://avatars.githubusercontent.com/u/48438513?v=4" alt="" class="w-100"></div>
+            <div class="col-4"><img src="https://avatars.githubusercontent.com/u/48438513?v=4" alt="" class="w-100"></div>
+            <div class="col-4"><img src="https://avatars.githubusercontent.com/u/48438513?v=4" alt="" class="w-100"></div>
+        </div>
     </div>
-</div>
 @endsection
