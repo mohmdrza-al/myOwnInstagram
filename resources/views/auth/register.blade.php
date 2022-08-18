c2328f54677be2ef7a3571d4ae0c459b6fcbadd8
mohmdrza <m.alagheband@gmail.com>
2022-08-18 11:47:11 +0430

    Updated login and fixed some bugs.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -28,6 +28,23 @@
                             </div>
 
                             <div class="row mb-3">
+                                <label for="username"
+                                       class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="username" type="username"
+                                           class="form-control @error('username') is-invalid @enderror" name="username"
+                                           value="{{ old('username') }}" required autocomplete="username">
+
+                                    @error('username')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
                                 <label for="email"
                                        class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
@@ -37,23 +54,6 @@
                                            value="{{ old('email') }}" required autocomplete="email">
 
                                     @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <label for="username"
-                                       class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="username" type="username"
-                                           class="form-control @error('username') is-invalid @enderror" name="email"
-                                           value="{{ old('username') }}" required autocomplete="username">
-
-                                    @error('username')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
